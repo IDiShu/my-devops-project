c794d64d881562b14c0e19f1d277139bd9d1be4f
IDiShu <andruhiv.vyacheslav@gmail.com>
2025-01-30 13:52:22 +0000

    Обновлен файл app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail, Message
 
+
 app = Flask(__name__)
 
 # Настройки базы данных (SQLite)
